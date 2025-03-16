6c5bdf156d642d436da6f834f147beed22f84ec8
Jared Moore <jared@jaredmoore.org>
2025-03-16 09:01:12 -0700

    update script for more fields.

--- local_scripts/download_google_scholar.py
+++ local_scripts/download_google_scholar.py
@@ -1,23 +1,36 @@
+"""
+Author: Jared Moore
+Date: March 2025
+
+Reads Nick's google scholar page and downloads the files in a bibtex format
+for this site.
+"""
+
 # Usage: python test.py > out.bib
 
+import pybtex.database
 from scholarly import scholarly
 
+# For markdown formatting
+output = "\n---\n---\n\n"
+
 # Search for an author by name
-search_query = scholarly.search_author('Nick Haber')
+search_query = scholarly.search_author("Nick Haber")
 author = next(search_query)
 
 # Fill in author details by fetching author data
 author = scholarly.fill(author)
 
 # Access the publications and export them to BibTeX format
-for pub in author['publications']:
+for pub in author["publications"]:
     # If the publication entry is not detailed, fill it up
-    if not pub.get('filled', False):
+    if not pub.get("filled", False):
         pub = scholarly.fill(pub)
-    
+
     # Use the full 'bib' info to construct and ensure the ID
     if "ENTRYTYPE" not in pub["bib"]:
         pub["bib"]["ENTRYTYPE"] = "article"
+
     if "ID" not in pub["bib"]:
         # Get first author's last name from the filled 'author' field
         author_str = pub["bib"].get("author", "unknown").lower()
@@ -34,12 +47,40 @@ for pub in author['publications']:
 
         # Get the publication year (after filling, this should now be present)
         year = pub["bib"].get("pub_year", "unknown")
-        
+
         # Construct the ID: lastName_firstWord_year
         pub["bib"]["ID"] = f"{last_name}_{first_word}_{year}"
-        
+
+        if year == "unknown":
+            print(f"No year in: {pub['bib']['ID']}")
+    if "pub_url" in pub:
+        pub["url"] = pub["pub_url"]
+        if pub["url"].startswith("https://arxiv.org/abs/"):
+            pub["pdf"] = pub["url"].replace("/abs/", "/pdf/")
+
     try:
-        print(scholarly.bibtex(pub))
-        print()
+        bibtex = scholarly.bibtex(pub)
+
+        parsed_bibtex = pybtex.database.parse_string(bibtex, "bibtex")
+        fields = list(parsed_bibtex.entries.values())[0].fields
+        if "url" in pub:
+            fields["url"] = pub["url"]
+        if "pdf" in pub:
+            fields["pdf"] = pub["pdf"]
+        if "author_pub_id" in pub:
+            fields["google_scholar_id"] = pub["author_pub_id"].split(":")[1]
+        if "pub_year" in pub:
+            fields["year"] = pub["pub_year"]
+        fields["bibtex_show"] = "true"
+        bibtex_str = parsed_bibtex.to_string("bibtex")
+        bibtex_str = bibtex_str.encode("ascii", errors="ignore").decode("ascii")
+
+        output += bibtex_str
+        output += "\n"
     except KeyError:
         pass
+
+output = output.replace("pub_year", "year").replace("\\", "")
+
+with open("papers.bib", "w") as f:
+    f.write(output)
